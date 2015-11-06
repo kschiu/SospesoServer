69f013f16a853db4d1f618944fa4308f73a530aa
Kevin Chiu <kschiu@andrew.cmu.edu>
2015-11-06 12:37:27 -0500

    Added relationship and validation to card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,9 @@
 class Card < ActiveRecord::Base
+  #relationship
+  belongs_to :user
+
+  #validations
+  validates :user_id, :card_number, :holder_name, :expiration_month, :expiration_year, :csv_code, presence: true
+  validates :zip_code, presence: true, format: { with: /\A\d{5}\z/, message: "Should be five digits long", allow_blank: true }
+
 end
