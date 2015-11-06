5a05cd6939cca757134c074d26dae5182e2673f1
Kevin Chiu <kschiu@andrew.cmu.edu>
2015-11-06 12:41:13 -0500

    Added relationship and validation to purchase

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,2 +1,8 @@
 class Purchase < ActiveRecord::Base
+  #relationships
+  has_one :card
+  belongs_to :user
+
+  #validations
+  validates :card_id, :user_id, numericality: {greater_than_or_equal_to: 0, only_integer: true }
 end
