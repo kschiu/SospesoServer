ee2f778dc619e6cbbfefe6883455485f0d00058e
Kevin Chiu <kschiu@andrew.cmu.edu>
2015-11-06 12:42:30 -0500

    Changed user_id validation to be numericality

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,7 +3,8 @@ class Card < ActiveRecord::Base
   belongs_to :user
 
   #validations
-  validates :user_id, :card_number, :holder_name, :expiration_month, :expiration_year, :csv_code, presence: true
+  validates :card_number, :holder_name, :expiration_month, :expiration_year, :csv_code, presence: true
   validates :zip_code, presence: true, format: { with: /\A\d{5}\z/, message: "Should be five digits long", allow_blank: true }
+  validates :user_id, numericality: {only_integer: true, greater_than_or_equal_to: 0}
 
 end
