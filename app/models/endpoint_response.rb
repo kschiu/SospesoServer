a939aa0fda440d808da4c45dbb13d42d4dde5279
Ben Lam <bklam@andrew.cmu.edu>
2015-12-02 16:01:46 -0500

    added some robustness to the api, can now get all related records for anything

--- app/models/endpoint_response.rb
+++ app/models/endpoint_response.rb
@@ -5,39 +5,46 @@ class EndpointResponse
     # this is general enough to encompass all endpoints
     # takes a string like "user" and converts it to the class User
     # so we can then do User.all, User.where(), etc.
-    temp_data = params[:endpoint].singularize.classify.constantize.all
+    klass = params[:endpoint].singularize.classify.constantize
+    @data = nil
     # return the related records for a specific id
     if !params[:id].nil? and !params[:related_endpoint].nil?
       # first assert that the attempted request for a relation is valid,
       # not a "destroy" or something, but a real association
-      all_ass_names = temp_data.reflect_on_all_associations.map{|q| q.class_name}
+      all_ass_names = klass.reflect_on_all_associations.map{|q| q.class_name}
       puts all_ass_names
       if !all_ass_names.include?(params[:related_endpoint].singularize.classify)
-        @data = nil
         @failed = "error, that's not a valid association of #{params[:endpoint]}"
       else
-        temp_user_list = temp_data.where('id = ?', params[:id])
+        temp_user_list = klass.where('id = ?', params[:id])
         if temp_user_list.length == 0
-          @data = nil
           @failed = "error, that user does not exist in the system"
         else
           # there should only be one user in the system with an id
           # need to check whether it should be pluralized or not
-          begin
-            @data = temp_user_list[0].send(params[:related_endpoint])
-          rescue
-            @data = temp_user_list[0].send(params[:related_endpoint].pluralize)
+          if klass.instance_methods.include?(params[:related_endpoint].to_sym)
+            actual_association = params[:related_endpoint]
+          elsif klass.instance_methods.include?(params[:related_endpoint].singularize.to_sym)
+            actual_association = params[:related_endpoint].singularize
+          elsif klass.instance_methods.include?(params[:related_endpoint].pluralize.to_sym)
+            actual_association = params[:related_endpoint].pluralize
+          else
+            @failed = "error, the related endpoint does not exist"
+            return @data
           end
+          #
+          @data = temp_user_list[0].send(actual_association)
           @failed = nil
         end
       end
     # otherwise, if there's a specified id, return that single record
     elsif !params[:id].nil?
-      @data = temp_data.where('id = ?', params[:id])
+    # there should only be one user in the system with an id
+      @data = klass.where('id = ?', params[:id])[0]
       @failed = nil
     # if there aren't any other specifications, just return all records
     else
-      @data = temp_data
+      @data = klass.all
       @failed = nil
     end
   
